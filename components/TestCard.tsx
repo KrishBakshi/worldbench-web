9eeed7f674ff1ff3f572d3b36aee1b0a7632e21b
Krish Bakshi <krishbakshi23@gmail.com>
2026-09-08 20:21:28 +0530

    Update: prefetch world HTML from test cards

--- components/TestCard.tsx
+++ components/TestCard.tsx
@@ -25,7 +25,7 @@ export default function TestCard({
   const wordmark = getProviderWordmark(test.provider);
 
   return (
-    <FastCard href={`/tests/${test.slug}`}>
+    <FastCard href={`/tests/${test.slug}`} documentHref={test.worldPreviewSrc}>
       <div
         className={`relative flex w-full flex-col items-center justify-center overflow-hidden px-4 ${
           compact ? "aspect-[4/3]" : "aspect-video"
